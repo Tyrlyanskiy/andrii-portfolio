a120ce06010d0057180864f558503f46f28b60a3
Andrii Diachenko <diachenko@deutsche-teilkauf.de>
2024-11-12 11:22:23 +0100

    feat: update bucket settings

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,8 +1,38 @@
 resource "aws_s3_bucket" "andrii-d-code-site" {
-  bucket = "andrii.d.code-documents"
-
+  bucket = var.andrii-d-code-site-bucket-name
   tags = {
-    Envirenment = "Prod"
+    Environment = "Prod"
   }
+}
 
+resource "aws_s3_bucket_ownership_controls" "public_access" {
+  bucket = aws_s3_bucket.andrii-d-code-site.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "andrii-d-code-site" {
+  bucket = aws_s3_bucket.andrii-d-code-site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.andrii-d-code-site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.andrii-d-code-site.arn}/*"
+      }
+    ]
+  })
 }
---
+++ infrastructure/variables.tf
@@ -0,0 +1,10 @@
+variable "andrii-d-code-site-bucket-name" {
+  description = "andrii.d.code-documents"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
